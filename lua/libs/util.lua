13bb7a61e18335b2eb37a52a98dd5bf5e47d1a06
Guthen <33220603+Guthen@users.noreply.github.com>
2020-09-20 12:32:32 +0200

    😎 New functions : image, collide and round

--- lua/libs/util.lua
+++ lua/libs/util.lua
@@ -45,6 +45,34 @@ function approach( inc, from, to )
     return to
 end
 
+--  @function round
+--      | description: Round a float to specified decimals number
+--      | params:
+--          number value: Float number to round
+--          number decimals: Number of decimals
+--      | return: number rounded
+function round( value, decimals )
+    local div = 10 ^ decimals
+    return math.floor( value * div ) / div
+end
+
+--  @function collide
+--      | description: Compute a AABB collision test on given coordinates
+--      | params:
+--          number a_x: X position of A object
+--          number a_y: Y position of A object
+--          number a_w: Width of A object
+--          number a_h: Height of A object
+--          number b_x: X position of B object
+--          number b_y: Y position of B object
+--          number b_w: Width of B object
+--          number b_h: Height of B object
+--      | return: boolean is_collide
+function collide( a_x, a_y, a_w, a_h, b_x, b_y, b_w, b_h )
+    return a_x < b_x + b_w and a_y < b_y + b_h 
+       and b_x < a_x + a_w and b_y < a_y + a_h
+end
+
 --  @function distance
 --      | description: Computes distance (in pixels) between two points
 --      | params:
@@ -83,4 +111,18 @@ function quads( image )
     end
 
     return quads
+end
+
+--  @function image
+--      | description: Load and cache the specified image
+--      | params:
+--          string path: Image path
+--      | return Image image
+local images = {}
+function image( path )
+    if not images[path] then
+        images[path] = love.graphics.newImage( "assets/images/" .. path )
+    end
+    
+    return images[path]
 end
