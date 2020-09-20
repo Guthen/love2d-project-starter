fcba5f53256ecd1176540f9b5b5db45e2fb03cfa
Guthen <33220603+Guthen@users.noreply.github.com>
2020-09-20 12:33:14 +0200

    ⌚ Timers: allow custom IDs

--- lua/libs/timer.lua
+++ lua/libs/timer.lua
@@ -1,3 +1,4 @@
+local id = 0
 Timers = {}
 
 --  @function timer
@@ -5,6 +6,7 @@ Timers = {}
 --      | params:
 --          number time: Time (in seconds)
 --          function callback: Callback to run
-function timer( time, callback )
-    Timers[#Timers + 1] = { max_time = time, time = 0, callback = callback }
+function timer( time, callback, custom_id )
+    Timers[custom_id or id] = { max_time = time, time = 0, callback = callback }
+    id = id + 1
 end
